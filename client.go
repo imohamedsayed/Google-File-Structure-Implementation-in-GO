c76d7b13d0205193380195129dab21ffd3cae360
Mohamed Sayed <mohamedelsayed0901@gmailcom>
2023-05-23 20:48:58 +0300

    Add Map Reduce

--- client.go
+++ client.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 var IPs [1]string // 1 IP for 1 slaves
+
+
+type KeyValue struct {
+    Key   string
+    Value int
+}
+type WordCount struct {
+	word  string
+	count int
+}
+
 
 func main() {
 	
@@ -35,7 +49,7 @@ func main() {
 		fmt.Scan(&file_name)
 	var result = ""
 	
-	for i := 0; i < 4; i++ { // 4 is the number of slaves
+	for i := 0; i < 1; i++ { // 4 is the number of slaves
 		conn, err := net.Dial("tcp", IPs[i]+":9090") 
 		if err != nil {
 			fmt.Println(err)
@@ -55,9 +69,19 @@ func main() {
 			fmt.Println(err)
 		}
 		result += (string(buf[:n]))
-	}
 
+		println(result)
+
+
+
+
+
+
+		println("------------------------------------")
+
+	}
 	f, err := os.Create("data.txt")
+
 
 	if err != nil {
 		fmt.Println(err)
@@ -65,10 +89,127 @@ func main() {
 
 	defer f.Close()
 
+
+	// Input data (list of sentences)
+	input := []string{
+		result,
+	}
+
+	// Split input into chunks for mapping
+	chunks := splitInput(input, 2)
+
+	// Map stage
+	mapOutput := performMap(chunks)
+
+	// Reduce stage
+	reduceOutput := performReduce(mapOutput)
+
+
 	_, err2 := f.WriteString(result)
+
+
+	// Print the word count results
+	for _, wc := range reduceOutput {
+
+		// fmt.Printf("%s: %d\n", wc.word, wc.count)
+
+		f.WriteString("\r")
+
+
+		count:= strconv.Itoa(wc.count)
+
+		
+		f.WriteString(wc.word + " : " +count)
+
+
+
+		// str := wc.word + " : " +  string(wc.count)
+
+		// f.WriteString(str)
+	}
+
+
+
 
 	if err2 != nil {
 		fmt.Println(err2)
 	}
 
+}
+
+
+// Splits the input data into chunks for mapping
+func splitInput(input []string, numChunks int) [][]string {
+	chunkSize := (len(input) + numChunks - 1) / numChunks
+	chunks := make([][]string, numChunks)
+	for i := 0; i < numChunks; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+		if end > len(input) {
+			end = len(input)
+		}
+		chunks[i] = input[start:end]
+	}
+	return chunks
+}
+
+// Map function
+func mapFunc(input string) []WordCount {
+	words := strings.Fields(input)
+	counts := make(map[string]int)
+	for _, word := range words {
+		counts[word]++
+	}
+	var wordCounts []WordCount
+	for word, count := range counts {
+		wordCounts = append(wordCounts, WordCount{word, count})
+	}
+	return wordCounts
+}
+
+// Performs the mapping phase
+func performMap(chunks [][]string) []WordCount {
+	var mapOutput []WordCount
+	var wg sync.WaitGroup
+	wg.Add(len(chunks))
+	for _, chunk := range chunks {
+		go func(chunk []string) {
+			defer wg.Done()
+			for _, input := range chunk {
+				mapOutput = append(mapOutput, mapFunc(input)...)
+			}
+		}(chunk)
+	}
+	wg.Wait()
+	return mapOutput
+}
+
+// Reduce function
+func reduceFunc(input []WordCount) WordCount {
+	word := input[0].word
+	count := 0
+	for _, wc := range input {
+		count += wc.count
+	}
+	return WordCount{word, count}
+}
+
+// Performs the reducing phase
+func performReduce(mapOutput []WordCount) []WordCount {
+	wordCounts := make(map[string][]WordCount)
+	for _, wc := range mapOutput {
+		word := wc.word
+		wordCounts[word] = append(wordCounts[word], wc)
+	}
+	var reduceOutput []WordCount
+	var wg sync.WaitGroup
+	wg.Add(len(wordCounts))
+	for _, counts := range wordCounts {
+		go func(counts []WordCount) {
+			defer wg.Done()
+			reduceOutput = append(reduceOutput, reduceFunc(counts))
+		}(counts)
+	}
+	wg.Wait()
+	return reduceOutput
 }
